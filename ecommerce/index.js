7e205be663bbb2039354804e3ce1357f6672484a
mohamed hamdy abdaltif <88945220+mohamed4211@users.noreply.github.com>
2022-09-21 18:08:09 +0200

    Add files via upload

---
+++ ecommerce/index.js
@@ -0,0 +1,22 @@
+function startTime() {
+    const today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('txt').innerHTML =  h + ":" + m + ":" + s;
+    setTimeout(startTime, 1000);
+  }
+  
+  function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+  }
+  startTime()
+  function login(){
+    location.replace("t1.html")
+  }
+  function sginin(){
+    location.replace("t2.html")
+  }
